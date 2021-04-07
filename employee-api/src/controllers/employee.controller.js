7239176a2011987d25f36f3c0f40bec32fd0ded2
William R. Ferreira <willian.usp@hotmail.com>
2021-04-06 22:15:34 -0300

    Desenvolvimento da rota 'listAllEmployees'

--- employee-api/src/controllers/employee.controller.js
+++ employee-api/src/controllers/employee.controller.js
@@ -8,17 +8,29 @@
 const db = require('../config/database')
 
 // ===> Método responsável por criar um novo 'Employee':
-exports.createEmployee = async(req, res) => {
-    const { name, job_role, salary, birth, employee_registration } = req.body
-    const { rows } = await db.query(
+exports.createEmployee = async (req, res) => {
+    const { name, job_role, salary, birth, employee_registration } = req.body;
+    try {
+      const { rows } = await db.query(
         "INSERT INTO employee (name, job_role, salary, birth, employee_registration) VALUES ($1, $2, $3, $4, $5)",
         [name, job_role, salary, birth, employee_registration]
-    )
-
-    res.status(201).send({
-        message: 'Employee adicionado com sucesso.',
+      );
+      res.status(201).send({
+        message: "Employee added successfully!",
         body: {
-            employee: {name, job_role, salary, birth, employee_registration}
-        }
-    })
-}    
+          employee: { name, job_role, salary, birth, employee_registration },
+        },
+      });
+    } catch (error) {
+      console.error('createEmployee', error);
+      res.status(500).send({
+        message: error 
+      });
+    }
+  };
+  
+// ===> Método responsável por listar todos os 'Employees':
+exports.listAllEmployees = async (req, res) => {
+    const response = await db.query('SELECT * FROM employee ORDER BY name ASC')
+    res.status(200).send(response.rows)
+}
--- employee-api/src/routes/employee.routes.js
+++ employee-api/src/routes/employee.routes.js
@@ -5,13 +5,15 @@
  * autor: William R. Ferreira
  */
 
-const router = require('express-promise-router')
-const { route } = require('.')
+const router = require('express-promise-router')()
 const employeeController = require('../controllers/employee.controller')
 
 // ===> Definindo as rotas do CRUD - 'employee'
 
-// ===> rota por criar um novo colaborador(a) : (POST): localhost:3000/api/employee
-route.post('/employee',employeeController.createEmployee)
+// ===> rota para criar um novo 'Colaborador(a)': (POST): localhost:3000/api/employees
+router.post('/employees', employeeController.createEmployee)
+
+// ===> rota responsável por listar todos os 'Colaboradores(as)': (GET): localhost:3000/api/employees
+router.get('/employees', employeeController.listAllEmployees)
 
 module.exports = router
