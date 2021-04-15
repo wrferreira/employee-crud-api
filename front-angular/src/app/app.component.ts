a4a1f2397293c0762ab8551309b88d4f04f61045
William R. Ferreira <willian.usp@hotmail.com>
2021-04-15 15:15:53 -0300

    Desenvolvimento do arquivo 'aapp.component.ts' em relação ao SlimLoadingBar

--- front-angular/src/app/app.component.ts
+++ front-angular/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { SlimLoadingBarService } from 'ng2-slim-loading-bar';
+import { Event, Router, NavigationStart, NavigationEnd, NavigationError, NavigationCancel  } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,32 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'front-angular';
+
+  /**
+   * Construtor responsável por trabalhar com a questão do LoadingBar na aplicação.
+   */
+  constructor(private SlimLoadingBarService: SlimLoadingBarService, private router: Router) {
+    this.router.events.subscribe((event: Event) => {
+        this.navigationInterceptor(event);
+    });
+  }
+
+
+  /**
+   *
+   * @param event Método responsável por tratar as condicionais em relação ao LoadingBar.
+   */
+  private navigationInterceptor(event: Event): void{
+    if(event instanceof NavigationStart){
+       this.SlimLoadingBarService.start();
+    }
+
+    if(event instanceof NavigationCancel || event instanceof NavigationError){
+      this.SlimLoadingBarService.stop();
+    }
+
+    if(event instanceof NavigationEnd){
+      this.SlimLoadingBarService.complete();
+    }
+  }
 }
