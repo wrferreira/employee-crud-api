1ecb523744614779d1fb9c700a17d2a2a6cc7f0a
William R. Ferreira <willian.usp@hotmail.com>
2021-04-15 14:07:45 -0300

    Adicionando um loading slim bar no projeto

--- front-angular/src/app/app-routing.module.ts
+++ front-angular/src/app/app-routing.module.ts
@@ -5,7 +5,11 @@ import { EmployeeAddComponent } from './employee-add/employee-add.component';
 import { EmployeeEditComponent } from './employee-edit/employee-edit.component';
 import { EmployeeGetComponent } from './employee-get/employee-get.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'employee/create', component: EmployeeAddComponent },
+  { path: 'employee/:id', component: EmployeeEditComponent },
+  { path: 'employee', component: EmployeeGetComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
